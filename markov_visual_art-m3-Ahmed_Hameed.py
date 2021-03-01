a914388ed35b58cb44b82707a150e6c18ac13656
Ahmed Hameed <hameedahmed433@gmail.com>
2021-03-01 16:28:06 -0500

    Markov Text Generation is complete! Just need to create visuals
    The Markov Chain portion of the project is complete, I have a system that reads in text and computes new text using 1-order Markov Chain probabilities. Probabilities are generated from frequency table of previous character occurrences.

--- markov_visual_art-m3-Ahmed_Hameed.py
+++ markov_visual_art-m3-Ahmed_Hameed.py
@@ -44,10 +44,11 @@ Outline:
 """
 
 import numpy as np
+import random as rand
 
 class MarkovTextArt:
 
-    def __init__(self, frequency_table={{}}, transition_matrix={}):
+    def __init__(self, frequency_table={}, transition_matrix={}):
         """
         Constructs a new MarkovTextArt class with a transition 
         """
@@ -55,20 +56,163 @@ class MarkovTextArt:
         # dictionary where a key is the current character and a value is a dictionary of previous characters
         # and their number occurrences
         self.frequency_table = frequency_table
+        
+        #self.all_chars = list(self.frequency_table.keys())
 
         # transition probabilites matrix based off of character occurrences in frequency table 
         # MIGHT JUST POPULATE USING A FUNCTION THAT RETURNS A TRANSITION MATRIX??
         self.transition_matrix = transition_matrix
     
+
     def read_string(self, words):
         """
-            Function that takes a string, words, and reads through each character while updating the
-            frequency table.
+        Function that takes a string, words, and reads through each character while updating the
+        frequency table.
         """
+        
+        # for every character in words that has a previous character (so starting at index 1)
         for i in range(1, len(words)):
             current_char = words[i]
             prev_char = words[i-1]
-            # NEED TO LOOK UP WHAT METHODS CAN BE CALLED ON A PYTHON DICT
-            prev_char_freq = frequency_table.get(current_char).get(prev_char)
-            frequency_table.get(current_char) = {prev_char: prev_char_freq+1}
 
+            # if character is in the frequency_table then just update the prev char occurrence
+            if current_char in self.frequency_table.keys():
+                prev_chars_dict = self.frequency_table.get(current_char)
+
+                # if the previous character already exists in the dictionary then update frequency by 1
+                if prev_char in prev_chars_dict.keys():
+                    self.frequency_table[current_char][prev_char] = prev_chars_dict[prev_char] + 1
+                # otherwise, create a new entry for the previous character
+                else:
+                    self.frequency_table[current_char][prev_char] = 1
+                    #self.frequency_table[current_char].update({prev_char: 1})
+            
+            # otherwise, create a new entry in the dictionary for the character
+            else:
+                self.frequency_table[current_char] = {prev_char: 1}
+
+
+    def read_from_file(self, filename):
+        """
+        Given a filename, reads the contents of that file and populates the frequency table, line by line.
+            Args: filename (str) - The name of the .txt file to be processed. 
+        """
+        text_file = open(filename, 'r')
+
+        # read entire file as a string and send to read_string()
+        text_str = text_file.read()
+        self.read_string(text_str)
+        print("File size ", len(text_str), "characters")
+        #for line in text_file:
+        #    self.read_string(line)
+        text_file.close()
+
+
+    def sum_char_occurences(self, char_dict):
+        """
+        Given a dictionary of characters and their occurrences, sums all of the occurrences and return
+        the total.
+            Args: char_dict (dict) - A dictionary of ASCII characters (keys) and their counts (values)
+            Return: char_total (int) - The sum of all the character occurrences in char_dict
+        """
+        char_total = 0
+
+        # iterating over every character count in char_dict
+        for val in char_dict.values():
+            char_total += val
+        
+        return char_total
+
+
+    def populate_transition_matrix(self):
+        """
+        Populates transition probabilites matrix based off of the frequency table.
+        Sums up all the previous character occurrences for a given character and turns them
+        into probabilities: frequency/sum of previous character occurrences = probability. 
+            Args: None.
+            Return: None.
+        """
+    
+        # iterate over every character key in frequency table
+        for key in self.frequency_table.keys():
+
+            prev_char_dict = self.frequency_table.get(key)
+            #print("Prev Char Dict:", prev_char_dict)
+            total_sum = self.sum_char_occurences(prev_char_dict)
+            #print("Total sum:", total_sum)
+            self.transition_matrix[key] = {}
+
+            # iterate over every previous character occurrence for current character key
+            for prev_char, count in prev_char_dict.items():
+                trans_prob = round(count/total_sum, 3)
+                self.transition_matrix[key].update({prev_char: trans_prob})
+
+
+    def get_next_char(self, current_char):
+        """
+        Given a character key, uses the frequency table of previous characters associated with that 
+        character to generate the next character.
+            Args: current_char (str) - The character key which will be used to access the character counts
+                                        in the frequency table.
+            Return: next_char (str) - The next character, generated from the transition probabilities 
+                                        created by random.choices() 
+        """
+        #print(current_char)
+        prev_chars_dict = self.frequency_table.get(current_char)
+        all_chars = list(prev_chars_dict.keys())
+        char_counts = list(prev_chars_dict.values())
+
+        next_char = ''.join(rand.choices(list(all_chars), weights=char_counts, k=1))
+        return next_char
+
+
+    def generate_markov_text(self, num_chars, start_string):
+        """
+        Uses a 1-order Markov chain (a matrix of characters and transition probabilites) to generate new
+        text based off the previous character occurrences. A 1-order Markov chain only looks the previous 
+        state. Creates new text of size num_chars, and the new text continues after the last character 
+        in start_string
+        Example: "This is a test string!!!!!!!!" --> At character '!': There's a 87.5% chance of the next
+                character being another '!' and a 12.5% of it being 'g'.
+            Args:
+                num_chars (int) - The number of characters to be generated by the function
+                start_string (str) - The start of the new text, last character will be used to generate
+                                        the new text. 
+            Return:
+                markov_text (str) - The new text generated by the 1-order Markov Chain and transition matrix
+        """
+
+        markov_text = "Start String: " + start_string + "\n"
+
+        current_char = start_string[-1]
+        for i in range(num_chars):
+            next_char = (self.get_next_char(current_char))
+            markov_text += next_char
+            current_char = next_char
+        
+        return markov_text
+
+            
+    def generate_markov_art(self, markov_text):
+        """
+        Reads through each character of markov_text and generates art based off of 
+        """       
+
+        
+
+def main():
+    testing = MarkovTextArt()
+    testing.read_from_file("/Users/Ahmed/Desktop/Junior Year/Spring 2021 Semester/Computational Creativity/CC Programming/M3-A_Markov_Distinction/arctic_monkeys_AM_album_song_lyrics.txt")
+    
+    print(testing.frequency_table)
+    print()
+    #testing.populate_transition_matrix()
+    #print(testing.transition_matrix)
+    print()
+    #print(testing.get_next_char('a'))
+
+    print(testing.generate_markov_text(2000, "t"))
+
+     
+if __name__ == "__main__":
+    main()
